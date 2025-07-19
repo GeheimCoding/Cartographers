b19cc848958bc3f2cbc10b5aeeb9872c5c8de5b8
GeheimCoding <geheim.coding@gmail.com>
2025-07-19 10:10:11 +0200

    Spawn map on the left side

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::sprite::Anchor;
+use bevy::sprite::SpriteImageMode::Scale;
 
 fn main() {
     App::new()
@@ -9,5 +11,21 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, window: Single<&Window>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite {
+            anchor: Anchor::BottomLeft,
+            image: asset_server.load("textures/maps/map_a.png"),
+            image_mode: Scale(ScalingMode::FitCenter),
+            custom_size: Some(window.size()),
+            ..default()
+        },
+        Transform::from_translation(window.size().extend(0.0) / -2.0),
+    ));
 }
